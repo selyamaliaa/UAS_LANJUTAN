8b4ac4a5e33c239fdf28b530a8aece92df97ddcb
ishelllllll <isheliarizuka@gmail.com>
2022-03-31 02:03:15 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UAS_LANJUTAN.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
